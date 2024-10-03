ec4d3136bfc75a0898a4cff0edd3ba2f82ccb467
Janbei16 <jannisbeichler@gmail.com>
2024-10-03 14:49:52 +0200

    Add Backend & Frontend

---
+++ Backend/Message_Tool/src/main/java/com/Beichler/Message_Tool/MessageToolApplication.java
@@ -0,0 +1,13 @@
+package com.Beichler.Message_Tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageToolApplication.class, args);
+	}
+
+}
---
+++ Backend/Message_Tool/src/test/java/com/Beichler/Message_Tool/MessageToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Beichler.Message_Tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
